26a35f98329a51336d4abdb269880728b343977b
Arasa <Arasa@DESKTOP-3F57J6C>
2016-12-12 09:35:26 +0100

    Extraigo el cálculo de los datos y la visualizacion en otro metodo

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -4,34 +4,20 @@
  */
 package lovivido;
 
-import javax.swing.JOptionPane;
+
 
 public class LoVivido {
 
 public static void main(String[] args) {
 
 
-int m;
-int d;
-int h;
-int a;
+
 
 Metodos datos=new Metodos();
+Metodos resultado=new Metodos();
 
-a = datos.pedirDatos();
-m = (a*12);
-d = (a*365);
-h = (d*24);
  
- JOptionPane.showMessageDialog(null,  "Meses: "  +m,"Numero de meses vividos de " ,JOptionPane.INFORMATION_MESSAGE);
-
-
- JOptionPane.showMessageDialog(null, "Días: " +d,"Numero de días vividos de ",JOptionPane.INFORMATION_MESSAGE);
-
-
- JOptionPane.showMessageDialog(null, "Horas: " +h,"Numero de horas vividos de ",JOptionPane.INFORMATION_MESSAGE);
-
-   System.exit (0);
+resultado.mostrar();
 
 }
    
